c04059907522ae04c9a583dcf30114e96598b431
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2019-12-26 02:20:47 +0530

    added matplotlib tutorial nb

---
+++ matplotlib/Part 09 Plotting real time data.py
@@ -0,0 +1,46 @@
+import random
+from itertools import count
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+plt.style.use('fivethirtyeight')
+
+# x_vals = []
+# y_vals = []
+# index = count()
+
+# def animate(i):
+#     x_vals.append(next(index))
+#     y_vals.append(random.randint(0, 5))
+#     plt.cla()
+#     plt.plot(x_vals, y_vals)
+
+
+# ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+# plt.tight_layout()
+# plt.show()
+
+
+# example
+
+
+def animate(i):
+    data = pd.read_csv('data.csv')
+    x = data['x_value']
+    y1 = data['total_1']
+    y2 = data['total_2']
+
+    plt.cla()
+
+    plt.plot(x, y1, label='Channel 1')
+    plt.plot(x, y2, label='Channel 2')
+
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+
+
+ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+
+plt.tight_layout()
+plt.show()
